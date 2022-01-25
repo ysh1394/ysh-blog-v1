0abcea6d8624b2b9d0db88b120c66621e120ca94
ysh1394 <ysh13941394@gmail.com>
2022-01-26 01:27:14 +0900

    fix: header style 변경

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -31,40 +31,59 @@ const Header = () => {
   // };
 
   return (
-    <header className="flex justify-between p-5 max-w-7xl mx-auto">
-      <div className="flex items-center space-x-5">
-        <Link href="/">
-          <img
-            className="w-44 object-contain cursor-pointer"
-            alt="#"
-            src="https://links.papareact.com/yvf"
-          />
-        </Link>
-        <div className="hidden md:inline-flex items-center space-x-5">
-          {menus.map((menu) => {
-            return (
-              <Link href={menu.route || '/'}>
-                <h3
-                  // onClick={moveRoute}
-                  key={menu.id}
-                  className={
-                    selected === menu.route
-                      ? 'text-white bg-green-600 px-4 py-1 rounded-full cursor-pointer'
-                      : 'hover:underline hover:text-green-600 cursor-pointer'
-                  }
-                >
-                  {menu.title}
-                </h3>
-              </Link>
-            );
-          })}
+    <header className="w-full sticky top-0 z-50 bg-white ">
+      <div className="container flex items-center justify-between p-3 mx-auto ">
+        <div className="w-1 flex md:hidden" />
+        <div className="flex items-center justify-between space-x-5">
+          <Link href="/">
+            <h1 className="w-100% px-1 text-2xl font-extrabold italic text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-purple-600 hover:from-transparent hover:to-purple-600 hover:ease-out hover:duration-100 hover:translate-x-1 md:text-2xl cursor-pointer">
+              SYUNG~ ZA
+              {/* Syuung~~Zaa~ */}
+            </h1>
+          </Link>
+
+          <div className="hidden md:inline-flex items-center space-x-5">
+            {menus.map((menu) => {
+              return (
+                <Link key={menu.id} href={menu.route || '/'}>
+                  <h3
+                    // onClick={moveRoute}
+
+                    className={
+                      selected === menu.route
+                        ? 'text-white bg-gradient-to-tr from-green-400 to-purple-600 px-4 py-1 rounded-full '
+                        : 'text-gray-500 cursor-pointer hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-green-400 hover:to-purple-600 hover:border-b-2  hover:border-purple-400 transition duration-150 ease-out hover:ease-in'
+                    }
+                  >
+                    {menu.title}
+                  </h3>
+                </Link>
+              );
+            })}
+          </div>
         </div>
-      </div>
-      <div className="flex items-center space-x-5 text-green-600">
-        <h3>Sign In</h3>
-        <h3 className="border px-4 py-1 rounded-full border-green-600">
-          Get Started
-        </h3>
+        <div className="hidden md:flex items-center space-x-5 text-green-600 ">
+          <h3 className="cursor-pointer hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-green-400 hover:to-purple-600 hover:border-b-2  hover:border-purple-400 transition duration-150 ease-out hover:ease-in">
+            Sign In
+          </h3>
+          <h3 className="border px-4 py-1 rounded-full border-green-600 cursor-pointer hover:text-purple-600 hover:text-transparent hover:bg-clip-text hover:bg-gradient-to-r hover:from-green-400 hover:to-purple-600 hover:border-purple-400 transition duration-150 ease-out hover:ease-in">
+            Go to Github
+          </h3>
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="flex md:hidden h-6 w-6 text-gray-400 hover:text-gray-600 cursor-pointer"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
       </div>
     </header>
   );
